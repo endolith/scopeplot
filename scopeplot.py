eafbf10f2bc93d59f25f909b6ecdd1f95cbef8f9
endolith <endolith@gmail.com>
2018-01-25 18:50:36 -0500

    SciPy's next_fast_len is public now; use that.

--- scopeplot.py
+++ scopeplot.py
@@ -10,6 +10,7 @@ import numpy as np
 from numpy import asarray, linspace, arange, pad
 import matplotlib.pyplot as plt
 from scipy.signal import resample
+from scipy.fftpack import next_fast_len
 
 # minimum resampling factor
 RS = 2
@@ -18,58 +19,6 @@ RS = 2
 def _ceildiv(a, b):
     """Ceiling integer division"""
     return -(-a // b)
-
-
-def _next_regular(target):
-    """
-    Find the next regular number greater than or equal to target.
-    Regular numbers are composites of the prime factors 2, 3, and 5.
-    Also known as 5-smooth numbers or Hamming numbers, these are the optimal
-    size for inputs to FFTPACK.
-
-    Target must be a positive integer.
-    """
-    assert abs(int(target)) == target
-
-    if target <= 6:
-        return target
-
-    # Quickly check if it's already a power of 2
-    if not (target & (target-1)):
-        return target
-
-    match = float('inf')  # Anything found will be smaller
-    p5 = 1
-    while p5 < target:
-        p35 = p5
-        while p35 < target:
-            # Ceiling integer division, avoiding conversion to float
-            # (quotient = ceil(target / p35))
-            quotient = -(-target // p35)
-
-            # Quickly find next power of 2 >= quotient
-            try:
-                p2 = 2**((quotient - 1).bit_length())
-            except AttributeError:
-                # Fallback for Python <2.7
-                p2 = 2**(len(bin(quotient - 1)) - 2)
-
-            N = p2 * p35
-            if N == target:
-                return N
-            elif N < match:
-                match = N
-            p35 *= 3
-            if p35 == target:
-                return p35
-        if p35 < match:
-            match = p35
-        p5 *= 5
-        if p5 == target:
-            return p5
-    if p5 < match:
-        match = p5
-    return match
 
 
 def _get_weights(N):
@@ -197,7 +146,7 @@ def scopeplot(x, width=800, height=400, range_=None, cmap=None, plot=None):
     MIN_PAD = 5   # TODO: what should this be?  Seems subjective.
 
     # Make input an optimal length for fast processing
-    pad_amount = _next_regular(N + MIN_PAD) - N
+    pad_amount = next_fast_len(N + MIN_PAD) - N
 
     x = pad(x, (0, pad_amount), 'constant')
 
